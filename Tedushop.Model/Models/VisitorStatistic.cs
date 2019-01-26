d8efc5ba9781019a43b4c5f3380cd8bd7ac87c73
lehonghieu1990 <44346838+lehonghieu1990@users.noreply.github.com>
2019-01-27 00:44:31 +0700

    add domain entity for project

---
+++ Tedushop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tedushop.Model.Models
+{
+    [Table("VisitorStatistics")]
+    class VisitorStatistic
+    {
+        [Key]
+        public Guid ID { set; get; }
+        public DateTime VisitedDate { set; get; }
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
