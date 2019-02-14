b5aecf419bc6b7314723d1b204a8f292e1ac6020
lehonghieu1990 <44346838+lehonghieu1990@users.noreply.github.com>
2019-02-14 21:29:45 +0700

    hoan thien tang service

---
+++ TeduShop.Service/FooterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tedushop.Model.Models;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+
+namespace TeduShop.Service
+{
+    public interface IFooterService
+    {
+        void Add(Footer footer);
+        void Update(Footer footer);
+        void Delete(int id);
+        IEnumerable<Footer> GetAll();
+        IEnumerable<Footer> GetAllPaging(int page, int pageSize, out int totalRow);
+        Post GetById(int id);
+        IEnumerable<Footer> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+        void SaveChanges();
+    }
+    class FooterService
+    {
+        FooterRepository _footerRepository;
+        IUnitOfWork _unitOfWork;
+
+        public FooterService(FooterRepository footerRepository, IUnitOfWork unitOfWork)
+        {
+            this._footerRepository = footerRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        
+    }
+}
---
+++ TeduShop.Service/PostCategoryService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tedushop.Model.Models;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+
+namespace TeduShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void add(PostCategory postCategory);
+        void Update(PostCategory postCategory);
+        void Delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+        PostCategory GetById(int id);
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMutil(X => X.Status && X.ParentID == parentId);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
